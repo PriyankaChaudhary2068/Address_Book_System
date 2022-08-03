eb85bf4a6f806b72e4c277a51512431a3d462774
chutki12 <priyankachaudhary2068>
2022-08-03 10:44:06 +0530

    ADDED : This Code For To Ability To Add A New Contact To Address Book

--- AddressBook.java
+++ AddressBook.java
@@ -1,15 +1,15 @@
 package com.bridgelabz.addressBookSystem;
 
-import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class AddressBook {
+	
 
-	        static List<Contact> contactList = new ArrayList<>();
+	        static List<Contact> contactList = new ArrayList<Contact>();
 		    static Scanner scanner = new Scanner(System.in);
-		    
 		    
 		    public static void addContact() {
 		        System.out.println(" Enter your first name :");
@@ -18,7 +18,7 @@ public class AddressBook {
 		        String lastName = scanner.nextLine();
 		        System.out.println(" Enter your city name : ");
 		        String city = scanner.nextLine();
-		        System.out.println("Enter your state  : ");
+		        System.out.println("Enter your state : ");
 		        String state = scanner.nextLine();
 		        System.out.println(" Enter your zip code : ");
 		        long zip = scanner.nextLong();
@@ -34,11 +34,28 @@ public class AddressBook {
 		    public static void main(String[] args)
 		    {
 		        System.out.println(" Welcome To The Address Book Program ");
-		        addContact();
-		        System.out.println(contactList);
+		        {
+		            Scanner scanner = new Scanner(System.in);
+		            int flag = 1;
+		            while (flag == 1)
+		            {
+		                System.out.println("\n Enter : 1.For Adding a Contact Details 2.Exit ");
+		                int choice = scanner.nextInt();
+		                switch (choice) {
+		                    case 1:
+		                        addContact();
+		                        break;
+		                    case 2:
+		                        flag = 0;
+		                        break;
+		                    default:
+		                        System.out.println("Enter a valid choice");
+		                        break;
+		                }
+		            }
+		            scanner.close();
+
+		            System.out.println(contactList);
+		        }
 		    }
-		}
-
-	
-
-
+        }
